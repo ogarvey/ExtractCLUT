ee9a47bec3f2a3356e2fcf2516d9e7d5c1987cfe
Oliver Garvey <ogarvey@gmail.com>
2024-01-18 16:44:25 +0000

    BOLT update

--- Games/Notes/BOLT.md
+++ Games/Notes/BOLT.md
@@ -5,3 +5,17 @@
 @0x0 - 0x42 0x4F 0x4C 0x54 = BOLT
 
 @0xC - 4 byte value representing eof
+
+0x18 - 4? byte value representing start of data? 0xC70
+
+0x20 - Start of 16 byte blocks 
+
+Block1: 00 00 00 2D ->  2d0
+        00 00 F1 8E     f18e 
+        00 00 0E B2     eb2
+        00 00 00 00     =10310
+
+Block2: 00 00 00 A2 ->  a20
+        00 01 E2 0E     1e20e
+        00 01 03 10     10310 <-- total of block1
+        00 00 00 00     =2ef3e
--- Helpers/BoltFileHelper.cs
+++ Helpers/BoltFileHelper.cs
@@ -7,9 +7,31 @@ namespace ExtractCLUT.Helpers
 {
     public static class BoltFileHelper
     {
-        public static int GetEndOfBoltFile(byte[] data)
+        public static int GetEndOfBoltData(byte[] data)
         {
             return BitConverter.ToInt32(data.Skip(0xC).Take(4).Reverse().ToArray(), 0);
+        }
+
+        public static int GetStartOfBoltData(byte[] data)
+        {
+            return BitConverter.ToInt32(data.Skip(0x18).Take(4).Reverse().ToArray(), 0);
+        }
+
+        public static List<int> GetBoltOffsets(byte[] data)
+        {
+            var offsets = new List<int>();
+
+            var value = GetStartOfBoltData(data);
+            offsets.Add(value);
+            var index = 0x20;
+            while (index < offsets[0])
+            {
+                value = BitConverter.ToInt32(data.Skip(index+8).Take(4).Reverse().ToArray(), 0);
+                offsets.Add(value);
+                index+=16;
+            }
+
+            return offsets;
         }
     }
 }
